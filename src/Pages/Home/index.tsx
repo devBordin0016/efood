36e58c44102e17c6a6a37a52b0e85adf7102358d
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-05-09 12:01:10 -0300

    Alterando ordem das tags

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -14,7 +14,7 @@ const restaurantes: RestaurantModel[] = [
     rating: '4.9',
     description:
       'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
-    infos: ['Japonesa', 'Destaque da Semana']
+    infos: ['Destaque da Semana', 'Japonesa']
   },
   {
     id: 2,
